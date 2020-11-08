b2a879082d67a2f0c866e741582310d851a391b8
linqi <linqi@qq.com>
2020-11-08 17:02:31 +0800

    项目起步 - Element-ui整合

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入element-ui
+import ElementUI from 'element-ui'
+// 导入element-ui的css
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 注册element-ui
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
